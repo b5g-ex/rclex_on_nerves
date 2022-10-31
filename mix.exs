9086078d2fb26227991a99f8fb7df95fa087cbde
s-hosoai <shintaro.hosoai@change-vision.com>
2022-10-31 13:25:54 +0900

    add circuits_i2c

--- mix.exs
+++ mix.exs
@@ -48,7 +48,8 @@ defmodule RclexOnNerves.MixProject do
       # bumps to Nerves systems. Since these include Linux kernel and Erlang
       # version updates, please review their release notes in case
       # changes to your application are needed.
-      {:nerves_system_rpi4, "~> 1.19", runtime: false, targets: :rpi4}
+      {:nerves_system_rpi4, "~> 1.19", runtime: false, targets: :rpi4},
+      {:circuits_i2c, "~> 1.0"}
     ]
   end
 
