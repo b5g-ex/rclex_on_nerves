cf726295efdae0f5b5d0ff41156425f1fb5abc08
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2022-11-01 10:15:33 +0900

    bump rclex to v0.8.0

--- mix.exs
+++ mix.exs
@@ -34,9 +34,7 @@ defmodule RclexOnNerves.MixProject do
       {:shoehorn, "~> 0.9.1"},
       {:ring_logger, "~> 0.8.5"},
       {:toolshed, "~> 0.2.26"},
-      {:rclex,
-       git: "https://github.com/rclex/rclex.git",
-       branch: "feature-add_tasks_for_ros2_resource-pojiro"},
+      {:rclex, "~> 0.8.0"},
       {:muontrap, "~> 1.0"},
 
       # Dependencies for all targets except :host
